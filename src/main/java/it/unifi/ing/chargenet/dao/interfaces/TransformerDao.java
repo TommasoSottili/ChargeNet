445f4268c03b72a136bea0fb5ddb05d5287ce1e8
lucamoracci <stialuca2004@gmaiil.com>
2026-05-12 12:58:24 +0200

    Update: dao classes updated

--- src/main/java/it/unifi/ing/chargenet/dao/interfaces/TransformerDao.java
+++ src/main/java/it/unifi/ing/chargenet/dao/interfaces/TransformerDao.java
@@ -1,8 +1,8 @@
 package it.unifi.ing.chargenet.dao.interfaces;
 
 import it.unifi.ing.chargenet.domain.infrastructure.PowerTransformer;
+import java.util.List;
 
 public interface TransformerDao extends GenericDao<PowerTransformer> {
-    // Le 5 operazioni base del GenericDao sono sufficienti per avviare il GridCluster
-    // e caricare tutti i trasformatori della rete all'avvio.
+    List<PowerTransformer> findOverheated();
 }
